3ccd12ff5bf844e1bd1e7a862beb2b384497f91a
Spencer Shattuck <spencer.shattuck@gmail.com>
2015-09-11 22:56:34 -0600

    partially setting up app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CourtOfTheElvenFae
 The website for the Court of the Elven Fae
+
+The website for the Court of the Elven Fae runs on Node.js, is built on AngularJS with ui-router, and editable items are referenced on Firebase.
---
+++ angular/app.js
@@ -0,0 +1,103 @@
+var app = angular.module('cotef', ['ui.router', 'firebase']);
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "states/home/home.html",
+      controller: 'homeCtrl',
+      resolve: {
+        mainInfoRef: function(firebaseService){
+          return firebaseService.getMainInfo();
+        }
+      }
+    })
+    .state('about', {
+      url: "/about",
+      templateUrl: "states/about.html",
+      controller: "aboutCtrl"
+    })
+    .state('booking', {
+      url: "/booking",
+      templateUrl: "states/booking/booking.html",
+      controller: 'bookingCtrl'
+    })
+    .state('profiles', {
+      url: "/profiles",
+      templateUrl: "states/profiles/profiles.html",
+      controller: 'profilesCtrl',
+      resolve: {
+        profilesRef: function(firebaseService){
+          return firebaseService.getProfiles();
+        }
+      }
+    })
+    //Just the Admin header.
+    .state('admin', {
+      url: "",
+      templateUrl: "states/admin/admin.html",
+      abstract: true
+    })
+    //Login view
+    .state('admin.login', {
+      url: "/login",
+      templateUrl: "states/admin/login/login.html",
+      controller: 'loginCtrl'
+    })
+    //Where the edit nav buttons are held
+    .state('admin.edits', {
+      url: "",
+      templateUrl: "states/admin/edits.html",
+      controller: 'logoutCtrl',
+      abstract: true
+    })
+    .state('admin.edits.homeEdits', {
+      url: "/home-edits",
+      templateUrl: "states/admin/homeEdits/homeEdits.html",
+      controller: 'homeEditsCtrl',
+      resolve: {
+        loggedIn: function(authService){
+          return authService.checkAuth();
+        },
+        mainInfoRef: function(firebaseService, $state){
+          return firebaseService.getMainInfo();
+        }
+      }
+    })
+    .state('admin.edits.profilesEdits', {
+      url: "/profiles-edits",
+      templateUrl: "states/admin/profilesEdits/profilesEdits.html",
+      controller: 'profilesEditsCtrl',
+      resolve: {
+        loggedIn: function(authService){
+          return authService.checkAuth();
+        },
+        profilesRef: function(firebaseService, $state){
+        return firebaseService.getProfiles();
+        }
+      }
+    })
+
+    // For any unmatched url, redirect to "/"
+    .state('otherwise', {
+    url: '*path',
+    resolve: {
+      redirect: function($location) {
+        $location.path("/").replace()
+        console.log("redirecting...")
+      }
+    }
+    });
+
+    $locationProvider.html5Mode(true);
+
+})
+.run(function($rootScope, $state){
+  //If the route change failed due to authentication error, redirect them out
+  $rootScope.$on('$stateChangeError', function(current, previous, rejection){
+    console.log('You need to be logged in to see this page.');
+    
+      $state.go('admin.login');
+  })
+})
