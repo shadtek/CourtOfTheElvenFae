2266d0ca880de46a78e59e31e14ac83b006c68ef
shadtek <spencer.shattuck@gmail.com>
2016-05-03 20:54:14 -0600

    orderBy name works

--- states/home/courtCtrl.js
+++ states/home/courtCtrl.js
@@ -5,8 +5,7 @@ app.controller('courtCtrl', function($scope, firebaseService, profilesRef) {
 	
 	$scope.profiles = profilesRef;
 
-	$scope.showUserModal = function(idx){
-    var user = angular.copy($scope.profiles[idx]);
+	$scope.showUserModal = function(user){
     $scope.currUser = user;
     $('#myModalLabel').text(user.name);
     $('#myModal').modal('show');
